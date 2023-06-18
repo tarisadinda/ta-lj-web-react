72441070e3a0a237ca7807e0b85c0e359beb33db
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-18 15:11:58 +0700

    add route

--- src/config/routing/index.js
+++ src/config/routing/index.js
@@ -4,6 +4,7 @@ import Login from "../../pages/login";
 import LayoutAuth from "../../components/layouts/auth";
 import LayoutMain from "../../components/layouts/main";
 import Dashboard from "../../pages/dashboard";
+import AllCompany from "../../pages/company/all";
 
 const Routing = () => {
     return (<>
@@ -14,6 +15,7 @@ const Routing = () => {
                 </Route>
                 <Route element={<LayoutMain />}>
                     <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/company/all" element={<AllCompany />} />
                 </Route>
             </Routes>
         </Router>
