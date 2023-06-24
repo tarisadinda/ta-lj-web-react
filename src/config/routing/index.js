b7ca15a1337391d81bc3d85d7b2e72602f51d20e
tarisadinda20 <tarisadinda20@gmail.com>
2023-06-24 19:45:53 +0700

    add routing

--- src/config/routing/index.js
+++ src/config/routing/index.js
@@ -5,6 +5,8 @@ import LayoutAuth from "../../components/layouts/auth";
 import LayoutMain from "../../components/layouts/main";
 import Dashboard from "../../pages/dashboard";
 import AllCompany from "../../pages/company/all";
+import NewSubmission from "../../pages/company/new-submisson";
+import DetailCompany from "../../pages/company/detail-company";
 
 const Routing = () => {
     return (<>
@@ -16,6 +18,8 @@ const Routing = () => {
                 <Route element={<LayoutMain />}>
                     <Route path="/dashboard" element={<Dashboard />} />
                     <Route path="/company/all" element={<AllCompany />} />
+                    <Route path="/company/new-submission" element={<NewSubmission />} />
+                    <Route path="/company/detail-company" element={<DetailCompany />} />
                 </Route>
             </Routes>
         </Router>
